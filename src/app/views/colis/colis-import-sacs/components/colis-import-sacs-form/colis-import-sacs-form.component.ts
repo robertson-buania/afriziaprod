29b371d93cdeb17e8d3d43ce7a075c7ed8219cb3
Robertson <robertson.bilonda@hologram.cd>
2025-03-26 07:28:32 +0100

    'ok'

--- src/app/views/colis/colis-import-sacs/components/colis-import-sacs-form/colis-import-sacs-form.component.ts
+++ src/app/views/colis/colis-import-sacs/components/colis-import-sacs-form/colis-import-sacs-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal, HostListener, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, signal, HostListener, EventEmitter, Output, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FirebaseService } from '@/app/core/services/firebase.service';
