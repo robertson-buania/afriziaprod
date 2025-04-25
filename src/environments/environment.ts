2c43aeb191983cf577db3e34961e2213aba9bd8d
Robertson <robertson.bilonda@hologram.cd>
2025-04-25 05:47:01 +0100

    ok

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,26 +16,26 @@ export const environment = {
   ARAKA_PAYMENT_PAGE_ID: '091B4125-2471-4DF0-B696-F7C3AFFB3A1A',
   firebaseConfig:
 
-  // {
-  //   apiKey: "AIzaSyCR2yCWCWXjS17poimbW1xiIZJq5H9cGBQ",
-  //   authDomain: "jazkonza.firebaseapp.com",
-  //   projectId: "jazkonza",
-  //   storageBucket: "jazkonza.firebasestorage.app",
-  //   messagingSenderId: "749003564628",
-  //   appId: "1:749003564628:web:7c0fd37c6169c2fb0befc1",
-  //   measurementId: "G-F238KFG0XG"
-  // }
+  {
+    apiKey: "AIzaSyCR2yCWCWXjS17poimbW1xiIZJq5H9cGBQ",
+    authDomain: "jazkonza.firebaseapp.com",
+    projectId: "jazkonza",
+    storageBucket: "jazkonza.firebasestorage.app",
+    messagingSenderId: "749003564628",
+    appId: "1:749003564628:web:7c0fd37c6169c2fb0befc1",
+    measurementId: "G-F238KFG0XG"
+  }
 
 
 
-{
-  apiKey: "AIzaSyBxspYrMsyjpIinpuIjOqHMqS2cRX7cZ0c",
-  authDomain: "afriziaprod.firebaseapp.com",
-  projectId: "afriziaprod",
-  storageBucket: "afriziaprod.firebasestorage.app",
-  messagingSenderId: "609959722693",
-  appId: "1:609959722693:web:e075d926353c99358ffaac",
-  measurementId: "G-74R2400BTG"
-}
+// {
+//   apiKey: "AIzaSyBxspYrMsyjpIinpuIjOqHMqS2cRX7cZ0c",
+//   authDomain: "afriziaprod.firebaseapp.com",
+//   projectId: "afriziaprod",
+//   storageBucket: "afriziaprod.firebasestorage.app",
+//   messagingSenderId: "609959722693",
+//   appId: "1:609959722693:web:e075d926353c99358ffaac",
+//   measurementId: "G-74R2400BTG"
+// }
 
 };
